dc1e2cb89f81a4478b724d9670d6c2be3ad3a577
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-26 22:30:38 -0300

    confirmación automatizada por check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,7 +1,8 @@
 #ask user
 response=input('What is the Answer to the Great Question of Life, the Universe, and Everything? ').lower()
+
 #check
-if response == '42' or response == 'forty-two' or response =='forty two':
+if response.find('42') != -1 or response == 'forty-two' or response =='forty two':
     print('Yes')
 else:
     print('No')
