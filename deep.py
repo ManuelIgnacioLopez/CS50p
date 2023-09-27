227f2085dcc654dff5375976133bf027ed529648
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-26 22:28:13 -0300

    confirmación automatizada por check50 [check50=True]

--- deep.py
+++ deep.py
@@ -4,4 +4,4 @@ response=input('What is the Answer to the Great Question of Life, the Universe, 
 if response == '42' or response == 'forty-two' or response =='forty two':
     print('Yes')
 else:
-    print("No')
+    print('No')
