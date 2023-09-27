902c46558d821010e18079ccbffd61ae2a821b15
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-26 22:51:41 -0300

    confirmación automatizada por check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -6,8 +6,21 @@ found=False
 for i in extensions:
     if file_name.find(i) != -1:
         j=i.replace('.','')
-        print('image'+'/'+j, end='')
-        found=True
+        if i == 'jpg':
+            print('image/jpeg', end='')
+            found=True
+        elif i == 'txt':
+            print('text/plain', end='')
+            found=True
+        elif i == 'pdf':
+            print('application/pdf', end='')
+            found=True
+        elif i == 'zip':
+            print('application/zip', end='')
+            found=True
+        else:
+            print('image'+'/'+j, end='')
+            found=True
 
 if found == False:
     print("application/octet-stream")
