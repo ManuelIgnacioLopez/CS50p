c4af230e1953af81f0dd38d5de609381e9b1ba5b
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-30 12:39:20 -0300

    confirmación automatizada por check50 [check50=True]

--- grocery.py
+++ grocery.py
@@ -6,27 +6,34 @@
 #promt
 
 def main():
-    grocery_list=[]
+    grocery_list={}
     make_list(grocery_list)
+    print(grocery_list)
     print_list(grocery_list)
 
 
 
 #appends elements from user input to list
-def make_list(grocery_list):
+def make_list(dicty):
     while True:
         try:
             item=input()
-            grocery_list.append(item.upper())
+
+            if item.upper() not in dicty:
+                dicty[item.upper()]=1
+            else:
+                dicty[item.upper()]+=1
         except EOFError:
-            #print()
-            return grocery_list.sort()
+            print()
+            return dicty
 
 #list in order and result for user
-def print_list(grocery_list):
-    it=0
-    for item in grocery_list:
-        it+=1
-        print(f"{it} {item}")
+def print_list(dicty):
+
+    temp_list = [key for key in dicty]
+    temp_list= sorted(temp_list)
+    for key in temp_list:
+        print(f"{dicty.get(key)} {key}")
+
 
 main()
