118823302bea4181cfecdb6327ce52f95042561e
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-30 16:17:23 -0300

    confirmación automatizada por check50 [check50=True]

--- outdated.py
+++ outdated.py
@@ -1,26 +1,39 @@
 # from english format to ISO 8601
+#initialize c
+c=True
 # Months
 Months = {
-    "January": 1,
-    "February": 2,
-    "March": 3,
-    "April": 4,
-    "May": 5,
-    "June": 6,
-    "July": 7,
-    "August": 8,
-    "September": 9,
-    "October": 10,
-    "November": 11,
-    "December": 12
+    "January": '01',
+    "February": '02',
+    "March": '03',
+    "April": '04',
+    "May": '05',
+    "June": '06',
+    "July": '07',
+    "August": '08',
+    "September": '09',
+    "October": '10',
+    "November": '11',
+    "December": '12'
 }
 # prompt user
-user= input('Date: ').strip()
-if '/' in user:
+while c=True:
+    user= input('Date: ').strip()
+    if '/' in user:
     m,d,y=user.split('/')
-else:
-    m,d,y=user.split(' ')
-    m=m.replace(',','')
-    m=Months.get(m)
+    if int(d)<=31 and int(m)<=12:
+        c=False
+    else:
+        m,d,y=user.split(' ')
+        d=int(d)
+        m=m.replace(',','')
+        m=Months.get(m)
+        if d<=31 and int(m)<=12:
+            c=False
 # create date with new format
-print(f"{y}-{m}-{d}",end='')
+
+
+if d<=9:
+    print(f"{y}-{m}-0{d}",end='')
+else:
+    print(f"{y}-{m}-{d}",end='')
