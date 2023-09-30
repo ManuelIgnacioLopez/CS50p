63f3363bacdbe79228cc435b775fc9eb8bab593b
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-30 16:58:36 -0300

    confirmación automatizada por check50 [check50=True]

--- emojize.py
+++ emojize.py
@@ -1,2 +1,9 @@
 import emoji
-print(f'{emoji.emojize(input())}')
+try:
+    print(f'Output: {emoji.emojize(input("Input: "), language='alias')}')
+except Exception as e:
+    'idk'
+try:
+    print(f'Output: {emoji.emojize(input("Input: "))}')
+except Exception as e:
+    'idk'
