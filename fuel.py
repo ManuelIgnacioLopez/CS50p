28d5cb419c97418ac8b33b6192731258847ed93e
ManuelIgnacioLopez <ManuelIgnacioLopez@users.noreply.github.com>
2023-09-30 11:04:13 -0300

    confirmación automatizada por check50 [check50=True]

--- fuel.py
+++ fuel.py
@@ -1,5 +1,6 @@
+#Fuel Gauge
 """
-pThis is a program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is an integer,
+This is a program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is an integer,
     and then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank.
 If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty.
 And if 99% or more remains, output F instead to indicate that the tank is essentially full.
@@ -9,26 +10,27 @@ If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead pr
 
 #prompt for X/Y
 def main():
-    c=False
-    while c== False:
+    while True:
+        user_input= input("Fraction: ")
         try:
-            a, b = input("Fraction: ").split('/')
-            if isint(a) and isint(b):
-                if int(a)<= int (b):
-                    if int(b) != 0:
-                        if int(a)/int(b) <= 1/100:
-                            c=True
-                            return print('E')
-                        if int(a)/int(b) >= 99/100:
-                            c=True
-                            return print('F')
-                        else:
-                            c=True
-                            return print(f"{round(int(a)/int(b)*100)}%")
+            a, b = user_input.split('/')
+            try:
+                if isint(a) and isint(b):
+                    if int(a)<= int (b):
+                        if int(b) != 0:
+                            if int(a)/int(b) <= 1/100:
+                                return print('E')
+                            if int(a)/int(b) >= 99/100:
+                                return print('F')
+                            else:
+                                return print(f"{round(int(a)/int(b)*100)}%")
+            except Exception as e:
+                return print(e)
+        except ValueError:
+            mistake='not using /'
 
-        except Exception as e:
-           return print(e)
 
+#checks if str is fully int
 def isint(string):
     numbers=["1","2","3","4","5","6","7","8","9","0"]
     for i in string:
